fd2beae4a6621044aa93ea936ad666fda58bae99
sayemafatema <153305701+sayemafatema@users.noreply.github.com>
2026-04-23 14:31:34 +0530

    Task #4: SDK – sdk/src/index.ts

---
+++ sdk/src/index.ts
@@ -0,0 +1,7 @@
+export { MGLProvider } from './context/MGLContext';
+export { useMGL } from './context/MGLContext';
+export { MGLLaunchButton } from './components/MGLLaunchButton';
+export { MGLModal } from './components/MGLModal';
+export { MGLApp } from './components/MGLApp';
+
+export type { MGLConfig } from './context/MGLContext';
